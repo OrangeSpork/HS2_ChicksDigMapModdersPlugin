0403b324de023d3702de53efcdc15460367954cf
OrangeSpork <57274555+OrangeSpork@users.noreply.github.com>
2020-11-19 20:56:38 -0700

    Add project files.

---
+++ HS2_ChicksDigMapModdersPlugin/HS2ChicksDigMapModdersPlugin.cs
@@ -0,0 +1,92 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using HS2_ChicksDigMapModdersPlugin.Hooks;
+using Manager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HS2_ChicksDigMapModdersPlugin
+{
+    [BepInPlugin(GUID, PluginName, Version)]
+    [BepInProcess("HoneySelect2.exe")]
+    public class HS2ChicksDigMapModdersPlugin : BaseUnityPlugin
+    {
+
+        public const string GUID = "orange.spork.hs2chicksdigmapmoddersplugin";
+        public const string PluginName = "HS2ChicksDigMapModdersPlugin";
+        public const string Version = "1.0.0";
+
+        public static ConfigEntry<bool> UseModdedMapsForSafeEvents { get; set; }
+        public static ConfigEntry<bool> UseModdedMapsForAllEvents { get; set; }
+        public static ConfigEntry<bool> UseModdedMapsForFirstTimeGirls { get; set; }
+        public static ConfigEntry<bool> EnableDebugLogging { get; set; }
+
+
+        public static HS2ChicksDigMapModdersPlugin Instance { get; set; }
+
+        internal BepInEx.Logging.ManualLogSource Log => Logger;
+
+        public HS2ChicksDigMapModdersPlugin()
+        {
+            if (Instance != null)
+            {
+                throw new InvalidOperationException("Singleton Only.");
+            }
+
+            Instance = this;
+
+            UseModdedMapsForSafeEvents = Config.Bind("Options", "Use Modded Maps For Safe Events", true, new ConfigDescription("Girls Will Show Up In Modded Maps for Standard Events"));
+            UseModdedMapsForFirstTimeGirls = Config.Bind("Options", "Use Modded Maps For First Time girls", true, new ConfigDescription("Modded Maps are Selectable From Lobby for a First Meeting"));
+            UseModdedMapsForAllEvents = Config.Bind("Options", "Use Modded Maps For All Events", false, new ConfigDescription("Not Recommended...Really Not Recommended, Crashes Possible, Weirdity Guaranteed"));
+            EnableDebugLogging = Config.Bind("Options", "Enable Debug Logging", false, new ConfigDescription("Waste HD Space"));
+
+            PatchMe();
+        }
+
+        public void PatchMe()
+        {
+            Harmony harmony = new Harmony(GUID);
+            harmony.PatchAll(typeof(GeneralHooks));
+        }
+
+        public static int[] ModdedMapIds()
+        {
+            List<int> moddedMapIds = new List<int>();
+            foreach (MapInfo.Param param in BaseMap.infoTable.Values)
+            {
+                if (param.No > 10000)
+                {
+                    moddedMapIds.Add(param.No);
+                }
+            }
+            return moddedMapIds.ToArray();
+        }
+
+        public static int[] SafeEvents = new int[] { 0, 1, 2, 19, 21, 22, 23, 24, 25, 26, 33 };
+        public static bool SafeEventCheck(int eventId)
+        {
+            return SafeEvents.Contains(eventId);
+        }
+
+        public static void DumpEventInfo()
+        {
+            foreach (EventContentInfoData.Param eventParam in Singleton<Game>.Instance.infoEventContentDic.Values)
+            {
+                HS2ChicksDigMapModdersPlugin.Instance.Log.LogInfo(string.Format("Event {0} Name {1} Goto [{2}] Meeting [{3}]", eventParam.id, string.Join(":", eventParam.eventNames), string.Join(",", eventParam.goToFemaleMaps), string.Join(",", eventParam.meetingLocationMaps)));
+            }
+        }
+
+        public static void DumpMapInfo()
+        {
+            foreach (MapInfo.Param param in BaseMap.infoTable.Values)
+            {
+                HS2ChicksDigMapModdersPlugin.Instance.Log.LogInfo(string.Format("Map {0} Name {1}", param.No, string.Join(":", param.MapNames)));
+            }
+        }
+
+    }
+}
---
+++ HS2_ChicksDigMapModdersPlugin/Hooks/GeneralHooks.cs
@@ -0,0 +1,148 @@
+﻿using HarmonyLib;
+using HS2;
+using Illusion.Extensions;
+using Manager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static HS2.GlobalHS2Calc;
+
+namespace HS2_ChicksDigMapModdersPlugin.Hooks
+{
+    public static class GeneralHooks
+    {
+        private static BepInEx.Logging.ManualLogSource Log = HS2ChicksDigMapModdersPlugin.Instance.Log;
+
+        [HarmonyPostfix, HarmonyPatch(typeof(LobbyMapSelectUI), "InitList")]
+        public static void InitList(int _eventNo, LobbyMapSelectInfoScrollController ___scrollCtrl)
+        {
+            if (HS2ChicksDigMapModdersPlugin.EnableDebugLogging.Value)
+            {
+                Log.LogInfo(string.Format("Checking Map Visibility for Event {0}", _eventNo));
+            }
+            if (_eventNo == 24 && HS2ChicksDigMapModdersPlugin.UseModdedMapsForFirstTimeGirls.Value)
+            {
+                int[] array = Singleton<Game>.Instance.infoEventContentDic[_eventNo].meetingLocationMaps.AddRangeToArray(HS2ChicksDigMapModdersPlugin.ModdedMapIds());
+                List<MapInfo.Param> maps = BaseMap.infoTable.Values.Where((MapInfo.Param map) => map.Draw != -1).ToList();
+                array = GlobalHS2Calc.ExcludeAchievementMap(array);
+                array = GlobalHS2Calc.ExcludeFursRoomAchievementMap(array);
+                maps = GlobalHS2Calc.ExcludeAppendMap(maps);
+                ___scrollCtrl.SelectInfoClear();
+                ___scrollCtrl.Init(maps, array);
+            }
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(GlobalHS2Calc), "SelectMapID")]
+        public static void SelectMapIDPostfix(CharaInfo _charaInfo, List<int> _useMap, HashSet<int> _used)
+        {
+            if (!HS2ChicksDigMapModdersPlugin.UseModdedMapsForSafeEvents.Value)
+            {
+                return;
+            }
+            else if (!HS2ChicksDigMapModdersPlugin.SafeEventCheck(_charaInfo.eventID) && !HS2ChicksDigMapModdersPlugin.UseModdedMapsForAllEvents.Value)
+            {
+                return;
+            }
+
+            if (HS2ChicksDigMapModdersPlugin.EnableDebugLogging.Value)
+            {
+                Log.LogInfo(string.Format("Before Selecting {0} map {1}-{5} useMap {2} used {3} available {4}", _charaInfo.chaFile.parameter.fullname, _charaInfo.mapID, string.Join(" ", _useMap), string.Join(" ", _used), string.Join(" ", Singleton<Game>.Instance.infoEventContentDic[_charaInfo.eventID].goToFemaleMaps), NameForMapId(_charaInfo.mapID)));
+            }
+
+            int[] goToFemaleMaps = Singleton<Game>.Instance.infoEventContentDic[_charaInfo.eventID].goToFemaleMaps;
+            goToFemaleMaps = ExcludeAchievementMap(goToFemaleMaps);
+            List<int> list = new List<int>();
+            if (!((IReadOnlyCollection<int>)(object)goToFemaleMaps).IsNullOrEmpty())
+            {
+                list = goToFemaleMaps.AddRangeToArray(HS2ChicksDigMapModdersPlugin.ModdedMapIds()).Except(_useMap).Shuffle().ToList();
+            }
+            bool flag = false;
+            for (int i = 0; i < list.Count; i++)
+            {
+                int num = list[i];
+                if (!_used.Contains(num))
+                {
+                    _used.Add(num);
+                    _charaInfo.mapID = num;
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag && list.Any())
+            {
+                _charaInfo.mapID = list.Shuffle().FirstOrDefault();
+            }
+
+            if (HS2ChicksDigMapModdersPlugin.EnableDebugLogging.Value)
+            {
+                Log.LogInfo(string.Format("After Selecting {0} map {1}-{5} useMap {2} used {3} available {4}", _charaInfo.chaFile.parameter.fullname, _charaInfo.mapID, string.Join(" ", _useMap), string.Join(" ", _used), string.Join(" ", list), NameForMapId(_charaInfo.mapID)));
+            }
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(GlobalHS2Calc), "SelectMapIDMultiple")]
+        public static void SelectMapIDMultiplePostfix(List<CharaInfo> _charaInfos, List<int> _useMap, HashSet<int> _used)
+        {
+            for (int i = 0; i < _charaInfos.Count; i += 2)
+            {
+                if (!HS2ChicksDigMapModdersPlugin.UseModdedMapsForSafeEvents.Value)
+                {
+                    continue;
+                }
+                else if (!HS2ChicksDigMapModdersPlugin.SafeEventCheck(_charaInfos[i].eventID) && !HS2ChicksDigMapModdersPlugin.UseModdedMapsForAllEvents.Value)
+                {
+                    continue;
+                }
+
+                if (HS2ChicksDigMapModdersPlugin.EnableDebugLogging.Value)
+                {
+                    Log.LogInfo(string.Format("Before Selecting {0} map {1}-{5} useMap {2} used {3} available {4}", _charaInfos[i].chaFile.parameter.fullname, _charaInfos[i].mapID, string.Join(" ", _useMap), string.Join(" ", _used), string.Join(" ", Singleton<Game>.Instance.infoEventContentDic[_charaInfos[i].eventID].goToFemaleMaps), NameForMapId(_charaInfos[i].mapID)));
+                }
+
+                int[] goToFemaleMaps = Singleton<Game>.Instance.infoEventContentDic[_charaInfos[i].eventID].goToFemaleMaps;
+                goToFemaleMaps = ExcludeAchievementMap(goToFemaleMaps);
+                List<int> list = new List<int>();
+                if (!((IReadOnlyCollection<int>)(object)goToFemaleMaps).IsNullOrEmpty())
+                {
+                    list = goToFemaleMaps.AddRangeToArray(HS2ChicksDigMapModdersPlugin.ModdedMapIds()).Except(_useMap).Shuffle().ToList();
+                }
+                bool flag = false;
+                for (int j = 0; j < list.Count; j++)
+                {
+                    int num = list[j];
+                    if (!_used.Contains(num))
+                    {
+                        _used.Add(num);
+                        _charaInfos[i].mapID = num;
+                        flag = true;
+                        break;
+                    }
+                }
+                if (!flag)
+                {
+                    _charaInfos[i].mapID = list.Shuffle().FirstOrDefault();
+                }
+                _charaInfos[i + 1].mapID = _charaInfos[i].mapID;
+
+                if (HS2ChicksDigMapModdersPlugin.EnableDebugLogging.Value)
+                {
+                    Log.LogInfo(string.Format("After Selecting {0} map {1}-{5} useMap {2} used {3} available {4}", _charaInfos[i].chaFile.parameter.fullname, _charaInfos[i].mapID, string.Join(" ", _useMap), string.Join(" ", _used), string.Join(" ", list), NameForMapId(_charaInfos[i].mapID)));
+                }
+            }
+
+        }
+
+        public static string NameForMapId(int id)
+        {
+            foreach (MapInfo.Param param in BaseMap.infoTable.Values)
+            {
+                if (param.No == id)
+                {
+                    return string.Join(":", param.MapNames);
+                }
+            }
+            return null;
+        }
+    }
+}
