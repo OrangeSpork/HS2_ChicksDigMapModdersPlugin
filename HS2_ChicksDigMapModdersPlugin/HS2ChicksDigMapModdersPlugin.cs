218f72ca6849c4c70bff3001b973c088ac118b1c
OrangeSpork <57274555+OrangeSpork@users.noreply.github.com>
2021-04-06 22:21:35 -0600

    Release Version Change

--- HS2_ChicksDigMapModdersPlugin/HS2ChicksDigMapModdersPlugin.cs
+++ HS2_ChicksDigMapModdersPlugin/HS2ChicksDigMapModdersPlugin.cs
@@ -18,7 +18,7 @@ namespace HS2_ChicksDigMapModdersPlugin
 
         public const string GUID = "orange.spork.hs2chicksdigmapmoddersplugin";
         public const string PluginName = "HS2ChicksDigMapModdersPlugin";
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.1";
 
         public static ConfigEntry<bool> UseModdedMapsForSafeEvents { get; set; }
         public static ConfigEntry<bool> UseModdedMapsForAllEvents { get; set; }
